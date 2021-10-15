7e948a74d6030080c853a0119f1e11c91695b156
enespusa <pusaenes@gmail.com>
2021-10-15 18:35:05 +0300

    modules

---
+++ modüller.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# =============================================================================
+# import math
+# print(dir(math))
+# help(math)
+# 
+# print(math.factorial(6))
+# #girilen değerden küçük en büyük değeri döner
+# print(math.floor(5.6))
+# # girilen değerden büyük en küçük değeri döner
+# print(math.ceil(5.6))
+# =============================================================================
+
+# =============================================================================
+# import math as matematik
+# 
+# print(matematik.factorial(6))
+# print(matematik.floor(5.6))
+# =============================================================================
+
+# =============================================================================
+# from math import *
+# print(factorial(5))
+# =============================================================================
+
+from math import floor,ceil
+
+print(floor(5.8))
